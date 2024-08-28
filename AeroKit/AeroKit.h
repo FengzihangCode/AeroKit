794ef64d5e720764c2ff05a5ec95c9841953402f
DannyFeng <FengzihangCode@outlook.com>
2024-08-28 20:23:50 +0800

    Initial Commit

---
+++ AeroKit/AeroKit.h
@@ -0,0 +1,18 @@
+//
+//  AeroKit.h
+//  AeroKit
+//
+//  Created by feng on 8/28/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AeroKit.
+FOUNDATION_EXPORT double AeroKitVersionNumber;
+
+//! Project version string for AeroKit.
+FOUNDATION_EXPORT const unsigned char AeroKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AeroKit/PublicHeader.h>
+
+
